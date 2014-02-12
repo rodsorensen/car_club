c18cc046b2d329632df7c7f52253eb0e3995ec90
Rod Sorensen <rodsorensen@hotmail.com>
2014-02-11 20:58:20 -0800

    initial structure for rails car club application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CarClub::Application.config.secret_key_base = '3693f1fcdc074ca894eef7fd681c7aa1162844d805d05b08a6367c8de8755f9604c8162f707f47b05c839e65ba40edb732c41f15a1f42358889f2b8435590d0b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CarClub::Application.config.session_store :cookie_store, key: '_car_club_session'
